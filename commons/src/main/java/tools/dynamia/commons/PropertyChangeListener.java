ddc6389b5239409502caf6a870e681e41d1ab200
Mario Serrano <mario@dynamiasoluciones.com>
2022-07-28 16:02:50 -0500

    New utility method

--- commons/src/main/java/tools/dynamia/commons/PropertyChangeListener.java
+++ commons/src/main/java/tools/dynamia/commons/PropertyChangeListener.java
@@ -3,6 +3,7 @@ package tools.dynamia.commons;
 /**
  * Listener for properties change form beans
  */
+@FunctionalInterface
 public interface PropertyChangeListener {
 
     void propertyChange(PropertyChangeEvent evt);
--- commons/src/main/java/tools/dynamia/commons/PropertyChangeSupport.java
+++ commons/src/main/java/tools/dynamia/commons/PropertyChangeSupport.java
@@ -31,4 +31,18 @@ public class PropertyChangeSupport implements PropertyChangeListenerContainer {
         final var src = source != null ? source : this;
         listeners.forEach(l -> l.propertyChange(new PropertyChangeEvent(propertyName, src, oldValue, newValue)));
     }
+
+    /**
+     * Add listener to all containers
+     *
+     * @param containers
+     * @param listener
+     */
+    public static void onPropertyChange(List<? extends PropertyChangeListenerContainer> containers, PropertyChangeListener listener) {
+        containers.forEach(i -> i.addPropertyChangeListener(listener));
+    }
+
+    public static void onPropertyChange(PropertyChangeListenerContainer container, PropertyChangeListener listener) {
+        container.addPropertyChangeListener(listener);
+    }
 }
