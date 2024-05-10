a30f4b3e6af4d0746822f97bf3a728c92af2783b
Mario Serrano Leones <mario@dynamiasoluciones.com>
2024-05-10 10:15:00 -0500

    reimplement loading config parameters

--- zk/src/main/java/tools/dynamia/zk/crud/cfg/ConfigViewRender.java
+++ zk/src/main/java/tools/dynamia/zk/crud/cfg/ConfigViewRender.java
@@ -18,18 +18,16 @@ package tools.dynamia.zk.crud.cfg;
 
 import org.zkoss.bind.Binder;
 import org.zkoss.zk.ui.Component;
-import org.zkoss.zul.Grid;
 import org.zkoss.zul.Label;
 import org.zkoss.zul.Textbox;
 import tools.dynamia.actions.ActionEvent;
 import tools.dynamia.commons.BeanUtils;
+import tools.dynamia.commons.logger.LoggingService;
+import tools.dynamia.commons.logger.SLF4JLoggingService;
+import tools.dynamia.domain.query.ApplicationParameters;
 import tools.dynamia.domain.query.Parameter;
 import tools.dynamia.domain.util.DomainUtils;
-import tools.dynamia.viewers.Field;
-import tools.dynamia.viewers.View;
-import tools.dynamia.viewers.ViewDescriptor;
-import tools.dynamia.viewers.ViewRenderer;
-import tools.dynamia.viewers.ViewRendererException;
+import tools.dynamia.viewers.*;
 import tools.dynamia.viewers.util.Viewers;
 import tools.dynamia.zk.BindingComponentIndex;
 import tools.dynamia.zk.converters.Util;
@@ -44,6 +42,7 @@ import java.util.List;
  */
 public class ConfigViewRender implements ViewRenderer<List<Parameter>> {
 
+    private final static LoggingService LOGGER = new SLF4JLoggingService(ConfigViewRender.class);
     public static final String PARAM_PARAMETER_CLASS = "parameterClass";
     public static final String PARAM_PARAMETER_NAME = "parameterName";
     public static final String PARAM_CACHEABLE = "cacheable";
@@ -54,7 +53,6 @@ public class ConfigViewRender implements ViewRenderer<List<Parameter>> {
 
     protected ConfigView newConfigView() {
         ConfigView formView = new ConfigView();
-
         return formView;
     }
 
@@ -62,25 +60,33 @@ public class ConfigViewRender implements ViewRenderer<List<Parameter>> {
     public View<List<Parameter>> render(ViewDescriptor descriptor, List<Parameter> value) {
         value = loadConfigValue(descriptor);
         ConfigView view = newConfigView();
-
-        view.setValueSupplier(() -> loadConfigValue(descriptor));
+        checkConfigLayout(descriptor);
         delegateRender(view, descriptor, value);
         createBindings(view, descriptor, value);
         view.setAutosaveBindings(true);
         view.setActionEventBuilder((source, params) -> new ActionEvent(view.getValue(), view));
+        view.setValueSupplier(() -> loadConfigValue(descriptor));
         view.setValue(value);
         view.updateUI();
+
         return view;
     }
 
-    private List<Parameter> loadConfigValue(ViewDescriptor descriptor) {
+    protected void checkConfigLayout(ViewDescriptor descriptor) {
+        if (!descriptor.getLayout().getParams().containsKey(Viewers.LAYOUT_PARAM_COLUMNS)) {
+            descriptor.getLayout().addParam(Viewers.LAYOUT_PARAM_COLUMNS, "4");
+        }
+    }
+
+    protected List<Parameter> loadConfigValue(ViewDescriptor descriptor) {
         List<Parameter> value = new ArrayList<>();
         descriptor.addParam(Viewers.PARAM_IGNORE_BINDINGS, true);
 
 
         for (Field field : descriptor.getFields()) {
             if (field.isVisible()) {
-                loadParam(field, value);
+                value.add(loadParam(field));
+
 
                 if (field.getComponent() == null || field.getComponent().equals("label")) {
                     field.setComponent("textbox");
@@ -95,7 +101,7 @@ public class ConfigViewRender implements ViewRenderer<List<Parameter>> {
         return value;
     }
 
-    private void createBindings(ConfigView view, ViewDescriptor descriptor, List<Parameter> value) {
+    protected void createBindings(ConfigView view, ViewDescriptor descriptor, List<Parameter> value) {
         for (Field field : descriptor.getFields()) {
             Component component = view.getFieldComponent(field.getName()).getInputComponent();
             createBinding(component, field, view.getBinder(), view, value);
@@ -132,7 +138,7 @@ public class ConfigViewRender implements ViewRenderer<List<Parameter>> {
         }
     }
 
-    private String getParamName(Field field) {
+    protected String getParamName(Field field) {
         String id = field.getViewDescriptor().getId();
         String paramName = id + "_" + field.getName();
         if (field.getParams().get(PARAM_PARAMETER_NAME) != null) {
@@ -141,30 +147,25 @@ public class ConfigViewRender implements ViewRenderer<List<Parameter>> {
         return paramName;
     }
 
-    private Parameter loadParam(Field field, List<Parameter> params) {
+    protected Parameter loadParam(Field field) {
         Class<? extends Parameter> parameterClass = getParameterClass(field.getViewDescriptor());
         String name = getParamName(field);
 
-        Parameter par = BeanUtils.newInstance(parameterClass);
-        par.setName(name);
-        par.setValue(field.getParams().get(PARAM_DEFAULT_VALUE) != null ? field.getParams().get(PARAM_DEFAULT_VALUE).toString() : "");
+        LOGGER.info("Loading config parameter: " + name + " - " + parameterClass);
+        Parameter par = ApplicationParameters.get().getParameter(parameterClass, name);
+        if (par == null) {
+            par = BeanUtils.newInstance(parameterClass);
+            par.setName(name);
+            par.setValue(field.getParams().get(PARAM_DEFAULT_VALUE) != null ? field.getParams().get(PARAM_DEFAULT_VALUE).toString() : "");
+        }
 
-        for (Parameter parameter : params) {
-            if (parameter.getName().equals(name)) {
-                par = parameter;
-                break;
-            }
-        }
-        if (par.getId() == null) {
-            params.add(par);
-        }
         par.setDescription(field.getDescription());
         par.setLabel(field.getLabel());
         par.setCacheable(field.getParams().get(PARAM_CACHEABLE) == Boolean.TRUE);
         return par;
     }
 
-    private Class<? extends Parameter> getParameterClass(ViewDescriptor descriptor) {
+    protected Class<? extends Parameter> getParameterClass(ViewDescriptor descriptor) {
         Class<? extends Parameter> parameterClass = DomainUtils.getDefaultParameterClass();
         String parameterClassName = (String) descriptor.getParams().get(PARAM_PARAMETER_CLASS);
         if (parameterClassName != null) {
