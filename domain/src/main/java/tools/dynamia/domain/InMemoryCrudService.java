7dcb006b1862d5dc497b45639f210ff67bcdee8d
Mario Serrano Leones <mario@dynamiasoluciones.com>
2024-06-17 17:12:26 -0500

    in memory CrudService implementation for testing only

---
+++ domain/src/main/java/tools/dynamia/domain/InMemoryCrudService.java
@@ -0,0 +1,293 @@
+package tools.dynamia.domain;
+
+import tools.dynamia.commons.BeanUtils;
+import tools.dynamia.commons.Callback;
+import tools.dynamia.commons.logger.LoggingService;
+import tools.dynamia.commons.logger.SLF4JLoggingService;
+import tools.dynamia.commons.reflect.ReflectionException;
+import tools.dynamia.domain.query.QueryCondition;
+import tools.dynamia.domain.query.QueryParameters;
+import tools.dynamia.domain.services.impl.AbstractCrudService;
+import tools.dynamia.domain.util.CrudServiceListener;
+import tools.dynamia.domain.util.DomainUtils;
+import tools.dynamia.domain.util.QueryBuilder;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Very simple in memory implementation of {@link tools.dynamia.domain.services.CrudService} use only for unit testing.
+ * Never use this class in production or something bad could happen.
+ */
+public class InMemoryCrudService extends AbstractCrudService {
+
+    private static LoggingService LOGGER = new SLF4JLoggingService(InMemoryCrudService.class);
+    private List<CrudServiceListener> listeners;
+    private Map<Class<?>, List<Object>> database = new ConcurrentHashMap<>();
+
+
+    public List<Object> getEntities(Class<?> entityClass) {
+        return database.computeIfAbsent(entityClass, k -> new ArrayList<>());
+    }
+
+    protected List<?> filter(QueryParameters parameters, List<?> objects) {
+        //TODO: filter objects  using parameters
+
+        if (parameters == null || parameters.isEmpty()) {
+            return objects;
+        } else {
+            List<?> filtered = objects;
+            for (Map.Entry<String, Object> entry : parameters.entrySet()) {
+                String property = entry.getKey();
+                Object value = entry.getValue();
+
+                filtered = filterByProperty(filtered, property, value);
+
+            }
+            objects = filtered;
+        }
+        return objects;
+    }
+
+    private List<?> filterByProperty(List<?> sample, String property, Object value) {
+        List<Object> result = new ArrayList<>();
+        for (Object bean : sample) {
+            if (bean != null) {
+                Object beanPropertyValue = null;
+
+                try {
+                    beanPropertyValue = BeanUtils.invokeGetMethod(bean, property);
+                } catch (ReflectionException e) {
+                    try {
+                        beanPropertyValue = BeanUtils.invokeBooleanGetMethod(bean, property);
+                    } catch (Exception ex) {
+                        LOGGER.warn("Cannot filter: " + ex.getMessage());
+                    }
+                }
+
+                if (beanPropertyValue != null) {
+                    if (value instanceof QueryCondition<?> queryCondition) {
+                        try {
+                            if (queryCondition.match(beanPropertyValue)) {
+                                result.add(bean);
+                            }
+                        } catch (UnsupportedOperationException e) {
+                            LOGGER.warn("QueryCondition " + value + " not supported");
+                        }
+                    } else if (beanPropertyValue.equals(value)) {
+                        result.add(bean);
+
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+
+    @Override
+    protected List<CrudServiceListener> getListeners() {
+        return listeners;
+    }
+
+    public void setListeners(List<CrudServiceListener> listeners) {
+        this.listeners = listeners;
+    }
+
+    @Override
+    public Serializable getId(Class entityClass, QueryParameters params) {
+        Serializable id = null;
+        Object result = findSingle(entityClass, params);
+        id = findId(result);
+
+        return id;
+    }
+
+    private static Serializable findId(Object result) {
+        Serializable id;
+        id = DomainUtils.findEntityId(result);
+        if (id == null) {
+            try {
+                id = (Serializable) BeanUtils.getFieldValue("id", result);
+            } catch (Exception e) {
+                LOGGER.error("Error find by id ", e);
+            }
+        }
+        return id;
+    }
+
+    @Override
+    public <T> T create(T t) {
+        if (t != null) {
+            var entities = getEntities(t.getClass());
+            entities.add(t);
+            try {
+                BeanUtils.setFieldValue("id", t, Long.valueOf(entities.size()));
+            } catch (Exception e) {
+                //ignore
+            }
+        }
+        return t;
+    }
+
+    @Override
+    public <T> T update(T t) {
+        return t;
+    }
+
+    @Override
+    public <T> void delete(T t) {
+        if (t != null) {
+            getEntities(t.getClass()).remove(t);
+        }
+    }
+
+    @Override
+    public void delete(Class type, Serializable id) {
+        List entities = getEntities(type);
+        try {
+            entities.removeIf(o -> id.equals(findId(o)));
+        } catch (Exception e) {
+            LOGGER.error("Error deleting by class and id", e);
+        }
+    }
+
+    @Override
+    public void deleteAll(Class type) {
+        getEntities(type).clear();
+    }
+
+    @Override
+    public void updateField(Object entity, String field, Object value) {
+        try {
+            BeanUtils.setFieldValue(field, entity, value);
+        } catch (Exception e) {
+            LOGGER.error("Error updating field  [" + field + "] of entity " + entity + " with value [" + value + "]");
+        }
+
+    }
+
+    @Override
+    public <T> List<T> findAll(Class<T> type) {
+        List entities = getEntities(type);
+        List<T> result = (List<T>) Collections.unmodifiableList(entities);
+        return result;
+    }
+
+    @Override
+    public <T> List<T> findAll(Class<T> type, String orderBy) {
+        return findAll(type);
+    }
+
+    @Override
+    public <T> List<T> find(Class<T> type, QueryParameters parameters) {
+        return (List<T>) filter(parameters, getEntities(type));
+    }
+
+    @Override
+    public <T> List<T> executeQuery(QueryBuilder queryBuilder, QueryParameters parameters) {
+        return (List<T>) find(queryBuilder.getType(), parameters);
+    }
+
+    @Override
+    public <T> List<T> executeQuery(QueryBuilder queryBuilder) {
+        return executeQuery(queryBuilder, queryBuilder.getQueryParameters());
+    }
+
+    @Override
+    public <T> List<T> executeQuery(String queryText) {
+        return List.of();
+    }
+
+    @Override
+    public <T> List<T> executeQuery(String queryText, QueryParameters parameters) {
+        return List.of();
+    }
+
+    @Override
+    public int execute(String queryText, QueryParameters parameters) {
+        return 0;
+    }
+
+    @Override
+    public <T> T findSingle(Class<T> type, String property, Object value) {
+        var result = filter(QueryParameters.with(property, value), getEntities(type));
+        return (T) result.stream().findFirst().orElse(null);
+    }
+
+    @Override
+    public <T> T findSingle(Class<T> entityClass, QueryParameters params) {
+        var result = filter(params, getEntities(entityClass));
+        return (T) result.stream().findFirst().orElse(null);
+    }
+
+    @Override
+    public <T> List<T> findByFields(Class<T> type, String param, String... fields) {
+        return (List<T>) getEntities(type);
+    }
+
+    @Override
+    public <T> List<T> findByFields(Class<T> type, String param, QueryParameters defaultParams, String...
+            fields) {
+        return (List<T>) getEntities(type);
+    }
+
+    @Override
+    public List getPropertyValues(Class<?> entityClass, String property) {
+        try {
+            return getEntities(entityClass).stream().map(o -> BeanUtils.getFieldValue(property, o))
+                    .toList();
+        } catch (Exception e) {
+            LOGGER.error("Error getting property values for " + entityClass + " [" + property + "]", e);
+            return List.of();
+        }
+    }
+
+    @Override
+    public List getPropertyValues(Class entityClass, String property, QueryParameters params) {
+        try {
+            return filter(params, getEntities(entityClass))
+                    .stream()
+                    .map(o -> BeanUtils.getFieldValue(property, o))
+                    .toList();
+        } catch (Exception e) {
+            LOGGER.error("Error getting property values for " + entityClass + " [" + property + "]", e);
+            return List.of();
+        }
+    }
+
+    @Override
+    public int batchUpdate(Class type, String field, Object value, QueryParameters params) {
+        return 0;
+    }
+
+    @Override
+    public int batchUpdate(Class type, Map<String, Object> fieldvalues, QueryParameters params) {
+        return 0;
+    }
+
+    @Override
+    public <T> T reload(T entity) {
+        return entity;
+    }
+
+    @Override
+    public <T> T executeProjection(Class<T> resultClass, String projectionQueryText, QueryParameters parameters) {
+        return null;
+    }
+
+    @Override
+    public void executeWithinTransaction(Callback callback) {
+
+    }
+
+    @Override
+    public Object getDelgate() {
+        return this;
+    }
+}
---
+++ domain/src/test/java/tools/dynamia/domain/InMemoryCrudServiceTest.java
@@ -0,0 +1,104 @@
+package tools.dynamia.domain;
+
+import org.junit.Assert;
+import org.junit.Test;
+import tools.dynamia.domain.query.QueryConditions;
+import tools.dynamia.domain.query.QueryParameters;
+import tools.dynamia.domain.services.CrudService;
+
+import java.util.List;
+
+public class InMemoryCrudServiceTest {
+
+
+    @Test
+    public void shouldCreateEntity() {
+        CrudService crudService = new InMemoryCrudService();
+        var entity = new SomeEntity();
+        entity.setName("Test");
+        var r = crudService.create(entity);
+
+        Assert.assertNotNull(r.getId());
+        Assert.assertFalse(crudService.findAll(SomeEntity.class).isEmpty());
+    }
+
+    @Test
+    public void shouldCreate_10_Entities() {
+        CrudService crudService = new InMemoryCrudService();
+
+        for (int i = 0; i < 10; i++) {
+            var entity = new SomeEntity();
+            entity.setName("Test " + i);
+            var r = crudService.create(entity);
+            Assert.assertNotNull(r.getId());
+        }
+
+        List<SomeEntity> result = crudService.findAll(SomeEntity.class);
+        Assert.assertEquals(result.size(), 10);
+    }
+
+    @Test
+    public void shouldFindFirstEntity() {
+        CrudService crudService = new InMemoryCrudService();
+
+        for (int i = 0; i < 10; i++) {
+            var entity = new SomeEntity();
+            entity.setName("Test " + i);
+            crudService.create(entity);
+        }
+
+        SomeEntity first = crudService.findFirst(SomeEntity.class);
+        Assert.assertNotNull(first);
+        Assert.assertEquals(first.getName(), "Test 0");
+    }
+
+    @Test
+    public void shouldFilterByParamters() {
+        CrudService crudService = new InMemoryCrudService();
+        createSamples(crudService);
+        List<SomeEntity> filtered = crudService.find(SomeEntity.class, QueryParameters.with("accountId", QueryConditions.eq(1L))
+                .add("active", true));
+
+
+        Assert.assertEquals(filtered.size(), 5);
+
+        filtered = crudService.find(SomeEntity.class, QueryParameters.with("age", 41));
+
+
+        Assert.assertEquals(filtered.size(), 1);
+    }
+
+    @Test
+    public void shouldDeleteAll() {
+        CrudService crudService = new InMemoryCrudService();
+        createSamples(crudService);
+        crudService.deleteAll(SomeEntity.class);
+        Assert.assertTrue(crudService.findAll(SomeEntity.class).isEmpty());
+    }
+
+    @Test
+    public void shouldUpdateEntity() {
+        CrudService crudService = new InMemoryCrudService();
+        var entity = crudService.create(new SomeEntity());
+        Assert.assertNotNull(entity.getId());
+
+        entity.setName("Test Entity");
+        entity.setAge(100);
+        var result = crudService.update(entity);
+
+        Assert.assertEquals(result.getAge(), 100);
+
+
+    }
+
+    private static void createSamples(CrudService crudService) {
+        for (int i = 0; i < 10; i++) {
+            var entity = new SomeEntity();
+            entity.setName("Test " + i);
+            entity.setAge(1 + (i * 10));
+            entity.setActive(i % 2 == 0);
+            entity.setAccountId(i % 2 == 0 ? 1L : 2L);
+            crudService.create(entity);
+        }
+    }
+}
