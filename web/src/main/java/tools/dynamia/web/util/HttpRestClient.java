4bba2f90b3130483720f916bd6eb96e8a615ca16
Mario Serrano <mario@dynamiasoluciones.com>
2025-11-26 16:27:07 -0500

    refactor HttpRestClient to use RestClient instead of RestTemplate for improved HTTP handling

--- web/src/main/java/tools/dynamia/web/util/HttpRestClient.java
+++ web/src/main/java/tools/dynamia/web/util/HttpRestClient.java
@@ -19,12 +19,10 @@ package tools.dynamia.web.util;
 
 
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 import java.nio.charset.StandardCharsets;
@@ -34,7 +32,7 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * {@link RestTemplate} wrapper with BASIC and Bearer authentication support
+ * {@link RestClient} wrapper with BASIC and Bearer authentication support
  */
 public class HttpRestClient {
 
@@ -42,68 +40,75 @@ public class HttpRestClient {
     private static final String APPLICATION_JSON = "application/json";
     public static final String AUTHORIZATION_HEADER = "Authorization";
 
-    private final String url;
+    private final String baseURL;
     private String username;
     private String password;
     private String bearerToken;
-    private RestTemplate rest;
+    private RestClient restClient;
     private final Map<String, String> headers = new HashMap<>();
 
     private MediaType contentType = MediaType.APPLICATION_JSON;
 
-    public HttpRestClient(String url) {
-        this(url, null, null, null);
+    public HttpRestClient(RestClient restClient) {
+        this.restClient = restClient;
+        this.baseURL = "";
     }
 
-    public HttpRestClient(String url, String username, String password) {
-        this(url, username, password, null);
+    public HttpRestClient(String baseURL) {
+        this(baseURL, null, null, null);
     }
 
-    public HttpRestClient(String url, String username, String password, RestTemplate restTemplate) {
-        this.url = url;
+    public HttpRestClient(String baseURL, String username, String password) {
+        this(baseURL, username, password, null);
+    }
+
+    public HttpRestClient(String baseURL, String username, String password, RestTemplate restTemplate) {
+        this.baseURL = baseURL;
         this.username = username;
         this.password = password;
-        this.rest = restTemplate;
 
-        if (this.rest == null) {
-            this.rest = new RestTemplate(new HttpComponentsClientHttpRequestFactory());
-        } else {
-            this.rest = restTemplate;
+        RestClient.Builder builder = RestClient.builder()
+                .baseUrl(baseURL)
+                .defaultHeaders(this::setHeaders);
+
+        if (restTemplate != null) {
+            builder.requestFactory(restTemplate.getRequestFactory());
         }
+
+        this.restClient = builder.build();
     }
+
 
     public void addHeader(String name, String value) {
         headers.put(name, value);
     }
 
-    HttpHeaders createHeaders() {
-        return new HttpHeaders() {{
-            if (username != null && password != null) {
-                String auth = username + ":" + password;
-                byte[] encodedAuth = Base64.getEncoder().encode(
-                        auth.getBytes(StandardCharsets.US_ASCII));
-                String authHeader = "Basic " + new String(encodedAuth);
-                set(AUTHORIZATION_HEADER, authHeader);
-            } else if (bearerToken != null) {
-                set(AUTHORIZATION_HEADER, "Bearer " + bearerToken);
-            }
-
-            setContentType(contentType);
-            headers.forEach(this::set);
-        }};
+    private void setHeaders(HttpHeaders headers) {
+        if (username != null && password != null) {
+            String auth = username + ":" + password;
+            byte[] encodedAuth = Base64.getEncoder().encode(auth.getBytes(StandardCharsets.US_ASCII));
+            headers.set(AUTHORIZATION_HEADER, "Basic " + new String(encodedAuth));
+        } else if (bearerToken != null) {
+            headers.set(AUTHORIZATION_HEADER, "Bearer " + bearerToken);
+        }
+        headers.setContentType(contentType);
+        this.headers.forEach(headers::set);
     }
 
     public Map get(String uri) {
         return get(uri, Map.class);
-
     }
 
     public <T> T get(String uri, Class<T> responseClass) {
-        return exchange(HttpMethod.GET, uri, null, responseClass);
+        return restClient.get()
+                .uri(uri)
+                .headers(this::setHeaders)
+                .retrieve()
+                .body(responseClass);
     }
 
     public List getList(String uri) {
-        return exchange(HttpMethod.GET, uri, null, List.class);
+        return getList(uri, Object.class);
     }
 
     /**
@@ -115,28 +120,38 @@ public class HttpRestClient {
      * @return
      */
     public <T> List<T> getList(String uri, Class<T> type) {
-
-        return exchange(HttpMethod.GET, uri, null, new ParameterizedTypeReference<>() {
-        });
+        return restClient.get()
+                .uri(uri)
+                .headers(this::setHeaders)
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<T>>() {
+                });
     }
-
 
     public Map post(String uri, Map<String, Object> body) {
         return post(uri, body, Map.class);
-
     }
 
     public <T> T post(String uri, Object body, Class<T> responseClass) {
-        return exchange(HttpMethod.POST, uri, body, responseClass);
+        return restClient.post()
+                .uri(uri)
+                .headers(this::setHeaders)
+                .body(body)
+                .retrieve()
+                .body(responseClass);
     }
 
     public Map put(String uri, Map<String, Object> body) {
         return put(uri, body, Map.class);
-
     }
 
     public <T> T put(String uri, Object body, Class<T> responseClass) {
-        return exchange(HttpMethod.PUT, uri, body, responseClass);
+        return restClient.put()
+                .uri(uri)
+                .headers(this::setHeaders)
+                .body(body)
+                .retrieve()
+                .body(responseClass);
     }
 
     public Map delete(String uri) {
@@ -144,25 +159,33 @@ public class HttpRestClient {
     }
 
     public <T> T delete(String uri, Class<T> responseClass) {
-        return exchange(HttpMethod.DELETE, uri, null, responseClass);
+        return restClient.delete()
+                .uri(uri)
+                .headers(this::setHeaders)
+                .retrieve()
+                .body(responseClass);
     }
 
     public <T> T exchange(HttpMethod method, String uri, Object body, Class<T> responseClass) {
-        HttpEntity entity = new HttpEntity<>(body, createHeaders());
-
-        ResponseEntity<T> response = rest.exchange(url + uri, method, entity, responseClass);
-        return response.getBody();
+        return restClient.method(method)
+                .uri(uri)
+                .headers(this::setHeaders)
+                .body(body)
+                .retrieve()
+                .body(responseClass);
     }
 
     public <T> T exchange(HttpMethod method, String uri, Object body, ParameterizedTypeReference<T> type) {
-        HttpEntity entity = new HttpEntity<>(body, createHeaders());
-
-        ResponseEntity<T> response = rest.exchange(url + uri, method, entity, type);
-        return response.getBody();
+        return restClient.method(method)
+                .uri(uri)
+                .headers(this::setHeaders)
+                .body(body)
+                .retrieve()
+                .body(type);
     }
 
-    public RestTemplate getRestTemplate() {
-        return rest;
+    public RestClient getRestClient() {
+        return restClient;
     }
 
     public String getBearerToken() {
