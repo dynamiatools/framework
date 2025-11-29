572a89520c9570d3df139e3ce011cb457230544c
Mario Serrano <mario@dynamiasoluciones.com>
2025-11-29 02:27:17 -0500

    feat: Update license information in dynamia-tools.js to Apache License 2.0
    Signed-off-by: Mario Serrano <mario@dynamiasoluciones.com>

--- zk/src/main/resources/static/dynamia-tools/js/dynamia-tools.js
+++ zk/src/main/resources/static/dynamia-tools/js/dynamia-tools.js
@@ -1,21 +1,18 @@
-
 /*
- * Copyright (C) 2009 - 2019 Dynamia Soluciones IT S.A.S - NIT 900302344-1
- * Colombia - South America
- * All Rights Reserved.
+ * Copyright (C) 2023 Dynamia Soluciones IT S.A.S - NIT 900302344-1
+ * Colombia / South America
  *
- * DynamiaTools is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License (LGPL v3) as
- * published by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * DynamiaTools is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with DynamiaTools.  If not, see <https://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 function changeHash(value) {
