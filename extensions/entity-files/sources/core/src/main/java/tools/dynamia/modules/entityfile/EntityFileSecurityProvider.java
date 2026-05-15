245f2594ef09b841b136c7629955231760f6c5e6
Mario Serrano <mario@dynamiasoluciones.com>
2026-05-15 15:56:42 -0500

    feat: enhance EntityFileSecurityProvider with access check and token generation methods

--- extensions/entity-files/sources/core/src/main/java/tools/dynamia/modules/entityfile/EntityFileSecurityProvider.java
+++ extensions/entity-files/sources/core/src/main/java/tools/dynamia/modules/entityfile/EntityFileSecurityProvider.java
@@ -2,6 +2,8 @@ package tools.dynamia.modules.entityfile;
 
 import tools.dynamia.modules.entityfile.domain.EntityFile;
 
+import java.util.Map;
+
 /**
  * Simple security provider interface to control access to entity files.
  * Implement this interface and register as a bean in the application context to control access to entity files.
@@ -11,11 +13,22 @@ import tools.dynamia.modules.entityfile.domain.EntityFile;
 public interface EntityFileSecurityProvider {
 
     /**
+     * Check if entity file has access
      *
-     * @param entityFile
-     * @return
+     * @param entityFile entity file
+     * @param params     request parameters
+     * @param headers    request headers
+     * @return allowed
      */
-    boolean canAccess(EntityFile entityFile);
+    boolean canAccess(EntityFile entityFile, Map<String, String> params, Map<String, String> headers);
+
+    /**
+     * Generate a token for the given entity file. This token can be used to grant temporary access to the file without requiring authentication.
+     *
+     * @param entityFile entity file
+     * @return token
+     */
+    String generateToken(EntityFile entityFile);
 
 
 }
